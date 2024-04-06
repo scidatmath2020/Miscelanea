895621ad41ead3f79b7757e39f15b325cc5094f4
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2024-04-06 17:09:15 -0600

    Add files via upload

---
+++ R/Varios/eclipse/eclipse.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(jpeg)
+library(ggpubr)
+
+## ruta donde se descarga el archivo sol.jpg
+setwd("C:\\Users\\Usuario\\Documents\\scidata\\miscelanea\\R\\otros\\eclipse")
+
+## Leer la imagen del sol
+img = readJPEG("sol.jpg")
+
+## Crear las lunas
+mi_data = data.frame(indice = 1:11,
+                     centro = -5:5,
+                     y=0)
+
+## Gráfico sin animación
+p = ggplot(mi_data, aes(centro, y)) +
+  background_image(img) +
+  geom_point(color="black",size=80) +
+  xlim(-5,5) +
+  ylim(-5,5)
+
+## Crear animación
+p + transition_time(indice)
+
+
+
+
+
